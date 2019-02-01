3e517d088f9d8441991c936fba76edc2f52f1ead
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-01-31 17:14:49 -0700

    finished patient POJO

--- src/main/java/model/Patient.java
+++ src/main/java/model/Patient.java
@@ -5,4 +5,25 @@ public class Patient extends Person{
     private Insurance insurance;
     private PaymentCard paymentCard;
 
+    public Patient(String name, String lastName, int uniqueId, String email, long phoneNumber, Insurance insurance, PaymentCard paymentCard) {
+        super(name, lastName, uniqueId, email, phoneNumber);
+        this.setInsurance(insurance);
+        this.setPaymentCard(paymentCard);
+    }
+
+    public Insurance getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(Insurance insurance) {
+        this.insurance = insurance;
+    }
+
+    public PaymentCard getPaymentCard() {
+        return paymentCard;
+    }
+
+    public void setPaymentCard(PaymentCard paymentCard) {
+        this.paymentCard = paymentCard;
+    }
 }
